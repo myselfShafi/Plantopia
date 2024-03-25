49444af9fdbdaf2ce7f97f4e55d4029b003557dd
shafi <116013717+myselfShafi@users.noreply.github.com>
2024-03-25 16:47:53 +0530

    analytics setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import MainLayout from "@/layouts/index";
 import "@/styles/globals.css";
 import { ThemeComponent } from "@/themes/theme";
 import { GetCurrentTitle } from "@/utils/getCurrentTitle";
+import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
@@ -29,6 +30,7 @@ export default function App({ Component, pageProps }) {
                 <AdBanner />
                 <MainLayout>
                   <Component {...pageProps} />
+                  <Analytics />
                 </MainLayout>
               </ThemeComponent>
             );
